8f3928eb8877e68c1668ea7710574b18a822f9d6
Nimshi Venkat <nmeripo@asu.edu>
2018-03-18 06:09:04 -0700

    Update kNN.m

--- kNN.m
+++ kNN.m
@@ -1,12 +1,12 @@
-%Loading dataset
+% Loading dataset
 load 'faces.mat'
 
-%Finding number of samples in test data and train data
+% Finding number of samples in test data and train data
 test_data_n = size(testdata,1);
 tr_data_n = size(traindata,1);
 
 
-%Precomputing cosine Distance from test samples to training samples
+% Precomputing cosine Distance from test samples to training samples
 for i = 1:test_data_n
     for k = 1:tr_data_n
         A(i, k) = cosineDistance(traindata(k,:),testdata(i,:));
@@ -14,7 +14,7 @@ for i = 1:test_data_n
 end
 
 
-%Precomputing cosine Distance from training samples to other training samples
+% Precomputing cosine Distance from training samples to other training samples
 for i = 1:tr_data_n
     for k = 1:tr_data_n
         C(i, k) = cosineDistance(traindata(k,:),traindata(i,:));
@@ -22,7 +22,7 @@ for i = 1:tr_data_n
 end
 
 
-%Intialize test_error and train_error vectors to store data to plot graph
+% Intialize test_error and train_error vectors to store data to plot graph
 test_error = [];
 tr_error = [];
 [t1 t2] = solution(1,A,C);
@@ -42,7 +42,7 @@ end
 x = linspace(0,100,11);
 x((x == 0)) = 1;
 
-%Plot graph                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+% Plot graph                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
 figure(1);
 plot(x,tr_error,'r',x,test_error,'b');
 title('Test Error and Train Error vs k neighbours')
